0f4b44f8eafb9b04e20252c18bc79c82eca4af2d
vishnuvardhanbadveli <badvelivishnuvardhan@gmail.com>
2026-02-27 08:45:00 +0530

    uc3 commited

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Convert string to character array
+        char[] characters = input.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
