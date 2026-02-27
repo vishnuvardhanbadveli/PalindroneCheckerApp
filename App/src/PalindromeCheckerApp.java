8c4139abae39ce8cbdabec9efe15d3ced6c28103
vishnuvardhanbadveli <badvelivishnuvardhan@gmail.com>
2026-02-27 09:19:28 +0530

    uc5 commited

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -7,7 +7,7 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a string : ");
         String input = sc.nextLine();
 
         Stack<Character> stack = new Stack<>();
